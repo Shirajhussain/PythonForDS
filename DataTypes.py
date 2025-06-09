6539f4e59a342dcafe5981a2d512ad3300b49815
Shirajhussain <shirajhussain2005@gmail.com>
2025-06-09 12:19:28 +0530

    DATA TYPES

---
+++ DataTypes.py
@@ -0,0 +1,186 @@
+#Data types
+#Int type
+
+print(123)
+print(type(123))
+print("----------------------")
+
+#float type
+
+print(123.321)
+print(type(123.321))
+print("----------------------")
+
+#string
+print('a')
+print("52")
+print(type("54"))
+print("----------------------")
+
+#Operators
+
+#addition
+
+print(3 + 2)
+print(2.5 + 4)
+print(2.5 + 2.5)
+print("Shiraj "+"Hussain")
+print("----------------------")
+
+
+#Subtraction
+print(3 - 2)
+print(2.5 - 4)
+print(2.5 - 2.5)
+print("----------------------")
+
+
+#Multiplication
+
+print(3 * 2)
+print(2.5 * 4)
+print(2.5 * 2.5)
+print("----------------------")
+
+
+#divide
+print(3 / 2)
+print(2.5 / 4)
+print(2.5 / 2.5)
+print("----------------------")
+
+
+#Floor division
+
+print(3 // 2)
+print(12 // 2.5)
+print(2.5 // 2.5)
+print("----------------------")
+
+
+#Modulo
+print(3 % 2)
+print(12 % 2.5)
+print(2.5 % 2.5)
+print("----------------------")
+
+
+#Exponential
+print(3 ** 2)
+print(12 ** 2.5)
+print(2.5 ** 2.5)
+print("----------------------")
+
+#abs
+print(abs(-3.5))
+print(abs(-35))
+print("----------------------")
+
+
+#Compare Operator
+# ==
+print(3==5)
+print(1.0 == 1)
+print('A' == 'a')
+print(True == 1)
+print(False == 0)
+print('--------')
+
+#!=
+print(3!=5)
+print(1.0 != 1)
+print('A' != 'a')
+print(True != 1)
+print(False != 0)
+print('--------')
+
+#>
+print(1<3)
+print(1.0>1)
+print(3 >True)
+print(False > 0)
+print("---------------")
+
+
+#<
+print(1>3)
+print(1.0<1)
+print(3 < True)
+print(False > 0)
+print("---------------")
+
+
+#<=
+print(1<=3)
+print(1.0<=1)
+print(3<=True)
+print(False <= 0)
+print("---------------")
+
+
+#>=
+print(1>=3)
+print(1.0>=1)
+print(3>=True)
+print(False>=0)
+print("---------------")
+
+
+#LOGICAL OPERATOR
+
+#AND
+print(True and True)
+print(False and True)
+print(False and False)
+print("---------------")
+
+#0r
+print(True or True)
+print(False or True)
+print(False or False)
+print("---------------")
+
+#NOT
+print(not True)
+print("---------------")
+
+
+
+
+#TYPECASTING
+
+#INT
+
+print(3.14)
+print(int(3.14))
+print(int('10'))
+print(int(False))
+print("---------------")
+
+
+#FLOAT
+print(float(3.14))
+print(float('10'))
+print(float(False))
+print("---------------")
+
+#STR
+print(str(10))
+print(str(3.14))
+print(str('10'))
+print(str(False))
+print("---------------")
+
+
+#bool
+print(bool(1))
+print(bool(1000000))
+print(bool(-1212.12))
+print(bool(0))
+print(bool(''))
+print(bool())
+print("---------------")
+
+
+#SOME EXCEPTION
+print(int(float('3.14')))
